d64b2788c0bfa03afd0c178075c27eb9ca7b651d
jrivera26333 <jrivera26333@gmail.com>
2020-03-01 22:59:55 -0600

    Able to move an object along trackable planes

---
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+/// <summary>
+/// By adding this attribute it will bring a ARRaycastManager
+/// </summary>
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject gameObjectToInstantiate;
+    GameObject spawnedObject;
+    ARRaycastManager _arRaycastManager;
+    Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    private void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if(_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            //Pose represents a rotation and position
+            var hitPose = hits[0].pose;
+
+            //Only will happen once
+            if (spawnedObject == null)
+            {
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
+            }
+            else
+                spawnedObject.transform.position = hitPose.position;
+        }
+    }
+}
