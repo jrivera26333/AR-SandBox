d3a76c5ce3c13d9eb52577120c85fd3eebae4ba7
jrivera26333 <jrivera26333@gmail.com>
2020-03-01 23:44:55 -0600

    Another Scene regarding AR Plane Tracking and Placement was very similar to the last sample I did...

---
+++ Assets/Scripts/PlacementController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlacementController : MonoBehaviour
+{
+    [SerializeField] GameObject placedPrefab;
+    ARRaycastManager arRayCastManager;
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    public GameObject PlacePrefab
+    {
+        get
+        {
+            return placedPrefab;
+        }
+        set
+        {
+            placedPrefab = value;
+        }
+    }
+
+    private void Awake()
+    {
+        arRayCastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition)) { return; }
+
+        if(arRayCastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+            Instantiate(placedPrefab, hitPose.position, hitPose.rotation);
+        }
+    }
+}
